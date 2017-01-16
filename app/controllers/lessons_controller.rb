aacb94acce3c9c6fe3369d7d1945cc0e36d3d8fc
asmentko <asmentko@gmail.com>
2017-01-16 16:36:25 +0000

    Trying to figure out authentication bug in heroku

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -7,10 +7,16 @@ class LessonsController < ApplicationController
 
   private
 
+  # def require_authorized_for_current_lesson
+  #   if current_user && current_user.enrolled_in?(@course)
+  #     link_to lesson_path(lesson)
+  #   else
+  #     redirect_to course_path(current_lesson.section.course), alert: 'Please enroll in this course to view the lessons.'
+  #   end
+  # end
+
   def require_authorized_for_current_lesson
-    if current_course.user != current_user.enrolled_in?(@course)
-      redirect_to course_path(current_lesson.section.course), alert: 'Please enroll in this course to view the lessons.'
-    else
+    if current_user && current_user.enrolled_in?(@course)
       link_to lesson_path(lesson)
     end
   end
